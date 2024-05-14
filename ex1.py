cdce824c4b466371fd6c93602c5f541d25b5db4c
Ian <ianmonteiromoreira@gmail.com>
2024-05-14 11:07:06 -0300

    ex1

--- ex1.py
+++ ex1.py
@@ -4,3 +4,15 @@ notas por parâmetro e exibe a mensagem “Você foi Aprovado!” ou “Você fo
 6.0 a média mínima para aprovação.
 """
 
+
+def calculo_media(n1, n2):
+    if (n1 + n2) / 2 >= 6:
+        return 'Você foi Aprovado!'
+    else:
+        return 'Você foi Reprovado!'
+
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+print(calculo_media(nota1, nota2))
