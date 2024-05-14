dc54113a9f57f8a1d94ddda56a7f21780f10fba4
Ian <ianmonteiromoreira@gmail.com>
2024-05-14 11:04:00 -0300

    Iniciando a lista de exercicios sobre funções

---
+++ ex1.py
@@ -0,0 +1,6 @@
+"""
+1. Escreva um programa para solicitar as notas de duas provas. Faça uma função que receba as duas
+notas por parâmetro e exibe a mensagem “Você foi Aprovado!” ou “Você foi Reprovado!”. Considere
+6.0 a média mínima para aprovação.
+"""
+
